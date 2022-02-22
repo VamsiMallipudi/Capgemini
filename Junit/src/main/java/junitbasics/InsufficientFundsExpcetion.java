bf45adf7feaa9821c033d00687f3f217bb541a44
VamsiMallipudi <85820488+VamsiMallipudi@users.noreply.github.com>
2022-02-22 15:26:26 +0530

    Add files via upload

---
+++ Junit/src/main/java/junitbasics/InsufficientFundsExpcetion.java
@@ -0,0 +1,11 @@
+package junitbasics;
+
+public class InsufficientFundsExpcetion extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InsufficientFundsExpcetion() {
+		
+		super();
+	}
+}
